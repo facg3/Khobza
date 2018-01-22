4b313df372e5269da2668555ae658aeca05796a9
yasminhillis <jasminehillis94@gmail.com>
2018-01-22 16:26:45 +0200

    edits and adding a runner file Relates #56

--- tests/db.test.js
+++ tests/db.test.js
@@ -1,19 +1,19 @@
 const test = require('tape');
-const queries = require('../model/query/auth.js');
-const dbconnection = require('../model/database/db_connection.js')
+const {insertData,selectData} = require('../src/model/query/auth.js');
+const dbconnection = require('../src/model/databse/db_connection.js')
 
 test('should insert data successfully', (t)=>{
-  isertData('fake', 'fake', 'fake', (err, data)=>{
+  insertData('fake',05955555, 'fake', (err, data)=>{
     t.equal(err, null, 'There are no errors when data is inserted')
-    t.deepEqual(data, [], 'data is inserted successfully')
+    t.deepEqual(data, [ { id: 1 } ], 'data is inserted successfully')
     t.end();
   })
 })
 
-test('shloud get all data from database', (t)=>{
-  selectData('fake', (err, data)=>{
+test('should get all data from database', (t)=>{
+  selectData(05955555, (err, data)=>{
     t.equal(err, null, 'There are no errors when getting all data')
-    t.equl(data.length > 0, true, 'all data is got successfully')
+    t.equal(data.length > 0, true, 'all data is got successfully')
     t.end();
 })
 })
